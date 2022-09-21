6c68016b725636225c200d32caf4200d7823de1b
asal <asalhnzd.1995@gmail.com>
2022-09-21 15:22:56 +0430

    init

---
+++ bsc.py
@@ -0,0 +1,61 @@
+from web3 import Web3
+import json
+
+import config
+
+bsc = "https://bsc-dataseed.binance.org/"
+web3 = Web3(Web3.HTTPProvider(bsc))
+
+
+
+
+print(web3.isConnected())
+
+address = "0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c"
+
+abi = json.loads('[{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],'
+                 '"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{'
+                 '"name":"guy","type":"address"},{"name":"wad","type":"uint256"}],"name":"approve","outputs":[{'
+                 '"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},'
+                 '{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],'
+                 '"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{'
+                 '"name":"src","type":"address"},{"name":"dst","type":"address"},{"name":"wad","type":"uint256"}],'
+                 '"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,'
+                 '"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"wad",'
+                 '"type":"uint256"}],"name":"withdraw","outputs":[],"payable":false,"stateMutability":"nonpayable",'
+                 '"type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"",'
+                 '"type":"uint8"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,'
+                 '"inputs":[{"name":"","type":"address"}],"name":"balanceOf","outputs":[{"name":"",'
+                 '"type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,'
+                 '"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,'
+                 '"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"dst",'
+                 '"type":"address"},{"name":"wad","type":"uint256"}],"name":"transfer","outputs":[{"name":"",'
+                 '"type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},'
+                 '{"constant":false,"inputs":[],"name":"deposit","outputs":[],"payable":true,'
+                 '"stateMutability":"payable","type":"function"},{"constant":true,"inputs":[{"name":"",'
+                 '"type":"address"},{"name":"","type":"address"}],"name":"allowance","outputs":[{"name":"",'
+                 '"type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"payable":true,'
+                 '"stateMutability":"payable","type":"fallback"},{"anonymous":false,"inputs":[{"indexed":true,'
+                 '"name":"src","type":"address"},{"indexed":true,"name":"guy","type":"address"},{"indexed":false,'
+                 '"name":"wad","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{'
+                 '"indexed":true,"name":"src","type":"address"},{"indexed":true,"name":"dst","type":"address"},'
+                 '{"indexed":false,"name":"wad","type":"uint256"}],"name":"Transfer","type":"event"},'
+                 '{"anonymous":false,"inputs":[{"indexed":true,"name":"dst","type":"address"},{"indexed":false,'
+                 '"name":"wad","type":"uint256"}],"name":"Deposit","type":"event"},{"anonymous":false,"inputs":[{'
+                 '"indexed":true,"name":"src","type":"address"},{"indexed":false,"name":"wad","type":"uint256"}],'
+                 '"name":"Withdrawal","type":"event"}]')
+
+contract = web3.eth.contract(address=address, abi=abi)
+main_address = '0x1DFaD3f4B8523676a975D9e44647B8d5ca086907'
+
+my_balance = contract.functions.balanceOf(main_address).call()
+print(web3.fromWei(my_balance, 'ether'))
+
+
+totalSupply = contract.functions.totalSupply().call()
+# print(totalSupply)
+print(web3.fromWei(totalSupply, 'ether'))
+
+print(contract.functions.name().call())
+print(contract.functions.symbol().call())
+
